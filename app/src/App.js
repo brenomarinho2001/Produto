fe607d5d04d755dbd9f969093a090c5eaf823521
brenomarinho2001 <74988159+brenomarinho2001@users.noreply.github.com>
2022-11-16 20:44:27 -0300

    produto

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cadastro from './Components/Basics/Pages/Cadastro';
 import Login from './Components/Basics/Pages/Login/Login';
+import ProdutosLista from './Components/ProdutosLista';
+import Footers from './Components/Basics/Footers';
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <Route path="/" element={<Login/>}></Route>
         <Route path="/Cadastro" element={<Cadastro/>}></Route>
         <Route path="/Login" element={<Login/>}></Route>
+        <Route path="/produtos" element={<ProdutosLista/>}></Route>
       </Routes>
     </BrowserRouter>
     </div>
