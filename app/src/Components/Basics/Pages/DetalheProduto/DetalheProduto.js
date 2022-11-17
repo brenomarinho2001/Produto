8d6e7853ec5d800c9464144cf0bb8ae378eb0a7a
iana <ianaoliveira@alu.ufc.br>
2022-11-17 10:30:10 -0300

    pagina home e detalhe produto

---
+++ app/src/Components/Basics/Pages/DetalheProduto/DetalheProduto.js
@@ -0,0 +1,13 @@
+import Footers from '../../Footers'
+import Header from '../../Header'
+import './Produto.css'
+
+export default function DetalheProduto () {
+    return (
+        <div className='container'> 
+            <Header style={{height:'20%'}}/>
+            <Footers/>
+        </div>
+        
+    )
+}
