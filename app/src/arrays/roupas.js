96ba9c55692f3226dce993aad333168e2684627e
Ruan Gabriel <ruangabes@gmail.com>
2022-11-30 10:45:05 -0300

    array roupas

---
+++ app/src/arrays/roupas.js
@@ -0,0 +1,24 @@
+var roupas = [
+
+{"nome" : "APOCALIPTICA",
+"descrição":"Pandemias? Terremoto ? Bomba atômica? Saudades do mozão?  O look apocalipta resolve amore, aqui você não tem apenas um cropped, mas uma calça assimétrica babado pra agir e enfrentar a vida neste mundo em declínio. Usando tecido 100% reciclável e elástico, é super confortável pra aquele show da sua banda favorita, que parece mais uma maratona de São Silvestre. Fim do mundo sim, mas com estilo né gata?",
+"preço": 89.90 ,
+"imagem_" : "",
+},
+{"nome" : "CHICLETINHO",
+"descrição":"*mastigando debochada* A chicletinho não gruda em ninguém não viu, a bicha sai solta pelas ruas vivendo a sua vida bélissima, o gloss dela é de babaloo! Feita com um tecido leve e descontraído o rosa desse look faz você estar sempre equilibrada entre a gostosa e a elegante! 100% recicladão tá bom fia? RAAAAAM! ",
+"preço": 59.90 ,
+"imagem_" : "",
+},
+{"nome" : "EMO IS NOT DEAD",
+"descrição":"welcome to my life amoreee, é feita pra você que não é apenas emo mas também gótica, vampira e rockeira e pagodeira. É perfeita pra lembrar daquela época em que o Brasil tinha um presidente e você podia sofrer por amor no recreio da escola e não por não ter o minimo e o básico de direitos humanos. Produzida por um jeans recicladão e tecido de algodão reciclado é uma roupa confortável e divertida pra noite! ",
+"preço": 49.90 ,
+"imagem_" : "",
+},
+{"nome" : "MORCEGONA POKEMONA ",
+"descrição":"Imagina só tu caçar pokemons durante o intervalo do rolê com essa roupa perfeita e pronta para o ataque! Aqui é close por cima de close, uma quebração de tabu gigante! O material da calça é 100% jeans reciclado com uma brusinha sensual e super confortável de algodão, ou seja você fecha o tempo mas não fica no frio, mas se ainda sentir frio, coloca uma jaqueta que fica um lacry, dica da vovó.",
+"preço": 89.90 ,
+"imagem_" : "",
+}
+
+]
