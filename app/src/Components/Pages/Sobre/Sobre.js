67e0e3ca59f09a2fdb71827c8ccebc0e3e26fb01
iana <ianaoliveira@alu.ufc.br>
2022-12-01 11:14:05 -0300

    mudancinhas

--- app/src/Components/Pages/Sobre/Sobre.js
+++ app/src/Components/Pages/Sobre/Sobre.js
@@ -1,25 +1,44 @@
 import Header from '../../Basics/Header_logada/Header_logada'
 import Footers from "../../Basics/Footer/Footers";
 
+import './Sobre.css'
+
 export default function Sobre () {
     return (
         <div className="container">
             <Header/>
-                <div className="content">
-                <h3 className="titlepag"> Sobre a verter </h3>
-                
-                <h3 className="subtitle"> Somos uma marca de vestuário digital e físico </h3>
+                <div className='content'>
+                <h3 className='titlepag'> Sobre a verter </h3>
 
-                <p className="text">
-                Nosso objetivo é apresentar uma nova maneira de se expressar através da <br/>
-                escolha de  uma roupa, por meio de uma produção sustentável. 
-                <br/>
-                <br/>
-                Além disso, estamos sempre à procura de entregar as melhores peças que <br/>
-                priorizam todos os tamanhos, ampliando as possibilidades de um guarda <br/>
-                roupa para um tom mais ousado e rebelde.
-                </p>
+                <div className='containertext'> 
+                    <h3 className='subtitle'> Somos uma marca de vestuário digital e físico </h3>
+
+                    <p className='text'>
+                    Nosso objetivo é apresentar uma nova maneira de se expressar através da <br/>
+                    escolha de  uma roupa, por meio de uma produção sustentável. 
+                    <br/>
+                    <br/>
+                    Além disso, estamos sempre à procura de entregar as melhores peças que <br/>
+                    priorizam todos os tamanhos, ampliando as possibilidades de um guarda <br/>
+                    roupa para um tom mais ousado e rebelde.
+                    </p>
                 </div>
+                <div className='imagecontainer'>
+                    <div className='imagemock' />
+                </div>
+
+                <div className='snap'>
+                <h3 className='subtitle2'> Experiência Única</h3>
+                <p className='text'>
+                Oferecemos o serviço onde sem a necesidade de vestir as roupas, você poderá vê-la em sua aparência, 
+                por meio de filtros, havendo a opção de compra da roupa física em determinados casos.
+                </p>
+                <button className='buttonsnap'>
+                    Experimente no Snapchat
+                </button>
+                </div>
+                
+            </div>
             <Footers/>
         </div>
     )
